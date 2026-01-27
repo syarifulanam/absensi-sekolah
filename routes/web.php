e0875124fd3c737eeeec7d3d08455c260959dac3
syarifulanam <em.syariful.anam@gmail.com>
2026-01-28 01:12:30 +0700

    revisi web.php

--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,24 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::middleware('guest')->group(function () {
-    Route::get('/login', function () {
-        return view('auth.login');
-    })->name('login');
+    Route::get('/login', fn() => view('auth.login'))->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login.post');
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-    Route::middleware('guest')->group(function () {
-        Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-        Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
-    });
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
 
     Route::get('/forgot-password', [ResetPasswordController::class, 'showForgotForm'])->name('forgot.password');
     Route::post('/forgot-password', [ResetPasswordController::class, 'sendResetLink'])->name('forgot.password.post');
+
     Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('reset.password');
     Route::post('/reset-password', [ResetPasswordController::class, 'reset'])->name('reset.password.post');
 });
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard')->middleware('auth');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
     Route::get('/student/my-card', [StudentController::class, 'myCard'])->name('student.mycard');
 
     Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
@@ -38,13 +36,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/attendance/monitoring', [AttendanceController::class, 'monitoring'])->name('attendance.monitoring');
     Route::get('/attendance/monitoring/data', [AttendanceController::class, 'monitoringData'])->name('attendance.monitoring.data');
     Route::put('/attendance/{attendance}/update', [AttendanceController::class, 'update'])->name('attendance.update');
-});
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::get('/users/{user}/reset-password', [UserController::class, 'resetPassword'])->name('users.reset-password');
-Route::put('/users/{user}/reset-password', [UserController::class, 'updatePassword'])->name('users.update-password');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/users/{user}/reset-password', [UserController::class, 'resetPassword'])->name('users.reset-password');
+    Route::put('/users/{user}/reset-password', [UserController::class, 'updatePassword'])->name('users.update-password');
+});
